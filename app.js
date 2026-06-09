9078eaa28d653a84f50f69c4c02761d75a0fc822
tomo3104 <tamzs052014@gmail.com>
2026-06-09 17:47:25 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -136,7 +136,6 @@ function buildCardEl(card) {
       <div class="card-links">
         <a class="link-btn link-amazon" href="https://www.amazon.co.jp/dp/${encodeURIComponent(card.asin)}" target="_blank" rel="noopener">Amazon</a>
         <a class="link-btn link-mercari" href="${escapeAttr(card.mercari_url)}" target="_blank" rel="noopener">メルカリ</a>
-        <a class="link-btn link-rakuma" href="https://fril.jp/search?q=${encodeURIComponent(card.model + ' 新品')}" target="_blank" rel="noopener">ラクマ</a>
       </div>
     </div>
   `;
