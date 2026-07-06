ee9a418336505685fc9316183630948fd2ab9b38
tomo3104 <tamzs052014@gmail.com>
2026-07-06 23:51:39 +0900

    userscript v1.7: バージョン番号更新

--- userscripts/mercari_asin_checker.user.js
+++ userscripts/mercari_asin_checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mercari ASIN Checker
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  メルカリ検索結果をASINリストと照合して仕入れ候補を表示（クローラーリサーチのグループ選択をチェックボックスで複数選択可能に）
 // @match        https://jp.mercari.com/*
 // @grant        GM_xmlhttpRequest
