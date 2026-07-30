17fbf176456cb09378a6a8f1e88ea1343ff6e45e
tomo3104 <tamzs052014@gmail.com>
2026-07-31 01:32:07 +0900

    v1.1 - API方式に変更

--- userscripts/mercari_desc_model_finder.user.js
+++ userscripts/mercari_desc_model_finder.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mercari Description Model Finder
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  タイトルに型番がない商品の説明文から型番を抽出してlist.jsonと照合
 // @match        https://jp.mercari.com/*
 // @grant        GM_xmlhttpRequest
