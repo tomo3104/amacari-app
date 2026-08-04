2c113f0fde9dc8d597266c5edd5faf6b8d378a6a
tomo3104 <tamzs052014@gmail.com>
2026-08-05 02:57:09 +0900

    v2.54: dlog storage error safe

--- userscripts/mercari_desc_model_finder.user.js
+++ userscripts/mercari_desc_model_finder.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mercari Description Model Finder
 // @namespace    http://tampermonkey.net/
-// @version      2.53
+// @version      2.54
 // @description  タイトルに型番がない商品の説明文から型番を抽出してlist.jsonと照合（同一オリジンiframe方式）
 // @match        https://jp.mercari.com/*
 // @grant        GM_xmlhttpRequest
